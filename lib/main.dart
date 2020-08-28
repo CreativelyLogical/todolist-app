a3eb3f3583684c9024045884110eb8adf78a5245
Umutjan Mahmut <umutjan2010@hotmail.com>
2020-08-28 11:45:52 -0700

    locked in portrait mode for the app on Android platforms

--- lib/main.dart
+++ lib/main.dart
@@ -45,6 +45,10 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return ChangeNotifierProvider(
       create: (context) => TaskData(),
       child: MaterialApp(
